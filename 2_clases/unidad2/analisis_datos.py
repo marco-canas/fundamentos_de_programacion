61f801fe3fff92a6dc10c872da35b02fa117f6e7
Marco Canas <marco.canas@udea.edu.co>
2026-09-10 16:29:12 -0500

    q

--- 2_clases/unidad2/analisis_datos.py
+++
@@ -1,9 +0,0 @@
-def limpiar(lista):
-    return [x for x in lista if x is not None]
-
-def escalar(lista):
-    max_val = max(lista)
-    return [x/max_val for x in lista]
-
-def media(lista):
-    return sum(lista)/len(lista)
